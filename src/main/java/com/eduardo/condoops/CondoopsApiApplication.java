148ebf7d63f732da3c33b3aeaa736f84680b35f1
Eduardo <eduardoadriano0013@gmail.com>
2026-08-02 21:51:08 -0300

    chore: initialize project and configure persistence

---
+++ src/main/java/com/eduardo/condoops/CondoopsApiApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.condoops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondoopsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondoopsApiApplication.class, args);
+	}
+
+}
